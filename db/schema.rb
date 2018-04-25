5e9c8cc2ed22e43ac3a224d2eb54cede01e165c2
ahadhossainapon <ahadhossain73@gmail.com>
2018-04-25 14:12:22 -0400

    edit categoty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420170714) do
+ActiveRecord::Schema.define(version: 20180424164730) do
+
+  create_table "article_categories", force: true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,6 +24,12 @@ ActiveRecord::Schema.define(version: 20180420170714) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
